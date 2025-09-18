722538d786c7fd247b1ae30f5af94d778b5cbde1
Marek Marcola <Marek.Marcola@gmail.com>
2025-09-18 16:15:03 +0200

    hdev.sh: add script
    Change-Id: I4a706356fae3a8eba8e98823d9f5799c28145da0

---
+++ hdev.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+VERSION_BIN="202509180061"
+
+ID="[${0##*/}]"
+
+INSTALL=0
+VERSION=0
+HELP=0
+
+declare -a ARGS1
+ARGS2=""
+
+while [ $# -gt 0 ]
+do
+  case $1 in
+    --inst*|-inst*)
+      INSTALL=1
+      shift
+      ;;
+    --vers*|-vers*)
+      VERSION=1
+      shift
+      ;;
+    -h|-help|--help)
+      HELP=1
+      shift
+      ;;
+    --)
+      shift
+      ARGS2=$*
+      break
+      ;;
+    *)
+      ARGS1+=("$1")
+      shift
+      ;;
+  esac
+done
+
+if [ $HELP -eq 1 ]; then
+  echo "$(basename $0) -install   # install"
+  echo "$(basename $0) -version   # version"
+  exit 0
+fi
+
+#
+# stage: VERSION
+#
+if [ $VERSION -eq 1 ]; then
+  echo "${0##*/}  $VERSION_BIN"
+  [[ "$VERSION_ENV" != "" ]] && echo "cdev.env $VERSION_ENV"
+  if [ $(type -t helm) ]; then
+    set -ex
+    helm version
+    { set +ex; } 2>/dev/null
+  fi
+  exit 0
+fi
+
+#
+# stage: INSTALL
+#
+if [ $INSTALL -eq 1 ]; then
+  if [ -f hdev.env ]; then
+    for d in /usr/local/etc; do
+      if [ -d $d ]; then
+        set -ex
+        rsync -ai hdev.env $d
+        { set +ex; } 2>/dev/null
+      fi
+    done
+  fi
+  if [ -f hdev.sh ]; then
+    for d in /usr/local/bin; do
+      if [ -d $d ]; then
+        set -ex
+        rsync -ai hdev.sh $d
+        { set +ex; } 2>/dev/null
+      fi
+    done
+  fi
+  exit 0
+fi
