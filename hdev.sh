13c1e80369ebe006dcfe605dae2abdc4da1fdf21
Marek Marcola <Marek.Marcola@gmail.com>
2026-04-19 21:55:30 +0200

    hdev.sh: add ansible installer
    Change-Id: I17f1fe039e3d6a70e839c6dbfe6707de1b43b1bc

--- hdev.sh
+++ hdev.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-VERSION_BIN="260407"
+VERSION_BIN="260419"
 
 SN="${0##*/}"
 ID="[$SN]"
 
-INSTALL=0
+INSTALL_RSYNC=0
+INSTALL_ANPB=0
+INSTALL_ANPB_HP="hdev"
 VERSION=0
+EVAL=0
 HELP=0
 
 declare -a ARGS1
 ARGS2=""
 
+s=0
+
 while [ $# -gt 0 ]; do
   case $1 in
-    --inst*|-inst*)
-      INSTALL=1
-      shift
-      ;;
     --vers*|-vers*)
       VERSION=1
+      shift
+      ;;
+    --inst*|-inst*)
+      INSTALL_RSYNC=1
+      shift
+      ;;
+    --anpb|-anpb)
+      INSTALL_ANPB=1
+      [[ -n "$2" && ${2:0:1} != "-" ]] && INSTALL_ANPB_HP="$2" && shift
+      shift
+      ;;
+    -x)
+      EVAL=1
       shift
       ;;
     -h|-help|--help)
@@ -42,8 +56,9 @@ done
 # stage: HELP
 #
 if [ $HELP -eq 1 ]; then
-  echo "$SN -install   # install"
-  echo "$SN -version   # version"
+  echo "$SN -version                  # version"
+  echo "$SN -install                  # install with rsync"
+  echo "$SN -anpb [host_pattern] [-x] # install with ansible"
   exit 0
 fi
 
@@ -62,11 +77,14 @@ if [ $VERSION -eq 1 ]; then
 fi
 
 #
-# stage: INSTALL
+# stage: INSTALL-RSYNC
 #
-if [ $INSTALL -eq 1 ]; then
+if [ $INSTALL_RSYNC -eq 1 ]; then
+  (( $s != 0 )) && echo; ((++s))
+  echo "$ID: stage: INSTALL-RSYNC"
+
   if [ -f hdev.env ]; then
-    for d in /usr/local/etc; do
+    for d in /usr/local/etc /pub/pkb/kb/data/999220-hdev/999220-000020_hdev_script /pub/pkb/pb/playbooks/999220-hdev/files; do
       if [ -d $d ]; then
         set -ex
         rsync -ai hdev.env $d
@@ -75,7 +93,7 @@ if [ $INSTALL -eq 1 ]; then
     done
   fi
   if [ -f hdev.sh ]; then
-    for d in /usr/local/bin; do
+    for d in /usr/local/bin /pub/pkb/kb/data/999220-hdev/999220-000020_hdev_script /pub/pkb/pb/playbooks/999220-hdev/files; do
       if [ -d $d ]; then
         set -ex
         rsync -ai hdev.sh $d
@@ -83,5 +101,31 @@ if [ $INSTALL -eq 1 ]; then
       fi
     done
   fi
+
+  exit 0
+fi
+
+#
+# stage: INSTALL-ANPB
+#
+if [ $INSTALL_ANPB -eq 1 ]; then
+  (( $s != 0 )) && echo; ((++s))
+  echo "$ID: stage: INSTALL-ANPB (EVAL=$EVAL)"
+
+  if [ ! $(type -t anpb) ]; then
+    echo "$ID: error: command not found: anpb"
+    exit 1
+  fi
+
+  if [ $EVAL -eq 0 ]; then
+    set -ex
+    anpb hdev_install.yml -e h=$INSTALL_ANPB_HP --check --diff
+    { set +ex; } 2>/dev/null
+  else
+    set -ex
+    anpb hdev_install.yml -e h=$INSTALL_ANPB_HP
+    { set +ex; } 2>/dev/null
+  fi
+
   exit 0
 fi
